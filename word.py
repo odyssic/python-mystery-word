2470cc18c7615a367db89b32521e75a8943c6545
odyssic <stevenbuddenjr@gmail.com>
2020-02-22 15:49:24 -0500

    troubleshooting guess wrong

--- word.py
+++ word.py
@@ -3,21 +3,30 @@ import random
 VOWELS = ["a", "e", "i", "o"]
 
 
-class Player:
+# class Player:
+
+#     def __init__(self):
+#         pass
+#         guess_count = 0
+#         guesses = []
+#         guess_count = self.guess_count
+
+
+# def count_guesses(self, guesses, guess_count):
+
+#     pass
+#     if Game.guess_wrong:
+#         self.guess_count = self.guess_count - 1
+
+
+class Game():
+
     def __init__(self):
-        pass
-
-    def count_guesses(self):
-        guesses = 0
-        guesses + 1
-
-
-class Game:
-
-    def __init__(self):
-        self.player = Player()
+        # self.player = Player()
         # self.open_file()
         self.start_game(self.open_file())
+        # Player()
+        self.split_word = split_word
 
     def open_file(self):
 
@@ -39,8 +48,6 @@ class Game:
         word_length = word_args[0]
         split_word = word_args[1]
         game_word = word_args[2]
-        guess_count = 8
-        letter_guesses = []
 
         print("This is a word guessing game.\nYou'll be given a random word, and you have 8 tries to guess that word.\n On each turn, you can guess a letter.\nAre you ready?")
 
@@ -48,8 +55,10 @@ class Game:
 
         if answer.lower() == "y":
 
+            #  reinsert guess count to print statement.
+
             print(
-                f"Great! I've picked a random word for you.\nIt has {word_length} characters. You have {guess_count} guesses remaining.")
+                f"Great! I've picked a random word for you.\nIt has {word_length} characters. You have  guesses remaining.")
 
             print(game_word)
 
@@ -70,6 +79,7 @@ class Game:
 
         letter = input('Please choose a letter: ')
         letter = letter.lower()
+
         if len(letter) > 1:
             print("Please only guess one letter per turn!")
         if not letter.isalpha:
@@ -80,7 +90,7 @@ class Game:
             if letter in split_word:
                 self.guess_right(letter, split_word)
             else:
-                self.guess_wrong(letter, guess_count)
+                self.guess_wrong(letter)
 
     def guess_right(self, letter, split_word):
         if letter in VOWELS:
@@ -88,28 +98,49 @@ class Game:
         else:
             print(f'Yes! There is a {letter} in the word!')
 
+        split_word.index(letter)
+
         self.validate_input(split_word)
 
-    def guess_wrong(self, letter, split_word, guess_count):
+    def guess_wrong(self, letter, split_word):
 
         if letter in VOWELS:
             print(f'Sorry, the word does not have a {letter}!')
         else:
             print(f'Sorry, the word does not have a {letter}!')
 
-        print(f'Sorry,the word does not have a {letter}')
-        guess_count - 1
-
         self.validate_input(split_word)
-
-        return guess_count
 
     def won_game(self):
         pass
         # you won the game!
         #
 
-    def lost_game(self):
+    def lost_game(self, split_word, word_length):
+
+        print("You lost this time! would you like to play again? ")
+        answer = input('"(Y)es" or "(N)o"?')
+
+        if answer.lower() == "y":
+
+            print(
+                f"Great! I've picked a random word for you.\nIt has {word_length} characters. You have  guesses remaining.")
+
+            print(game_word)
+
+            self.validate_input(split_word)
+
+            # print('this is working')
+
+            # print(random_word)
+
+        elif answer.lower() == "n":
+
+            print("Thank you for playing. We'll see you again soon! Goodbye!")
+            exit()
+
+        return word_length, split_word
+
         pass
         # you lost the game!
 
