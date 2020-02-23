6abbe9e52f2c46b4918b03c93aa35ed4777f177c
odyssic <stevenbuddenjr@gmail.com>
2020-02-23 12:33:14 -0500

    the join for the display works; still no winning,but you can LOSE

--- word.py
+++ word.py
@@ -88,28 +88,23 @@ class Game():
 
     def guess_right(self, letter, split_word, guess_count, hidden_word, guess_letters, data, random_word):
 
-        while hidden_word != split_word:
+        if letter in ANWORDS:
+            print(f'Yes! There is an {letter} in the word!')
+        else:
+            print(f'Yes! There is a {letter} in the word!')
 
-            if letter in ANWORDS:
-                print(f'Yes! There is an {letter} in the word!')
-            else:
-                print(f'Yes! There is a {letter} in the word!')
+        guess_letters.append(letter)
+        print('guess letters:', guess_letters)
 
-            guess_letters.append(letter)
-            print('guess letters:', guess_letters)
+        hidden_word = self.alter_hidden_word(
+            letter, split_word, guess_count, guess_letters, hidden_word, data, random_word)
 
-            hidden_word = self.alter_hidden_word(
-                letter, split_word, guess_count, guess_letters, hidden_word, data, random_word)
+        print('guessright', hidden_word)
 
-            print('guessright', hidden_word)
+        self.display_hidden_word(
+            split_word, guess_count, guess_letters, hidden_word, data, random_word)
 
-            self.display_hidden_word(
-                split_word, guess_count, guess_letters, hidden_word, data, random_word)
-
-            print('guessright', hidden_word, 'after alter hidden word')
-
-        if hidden_word == split_word:
-            self.won_game(data)
+        print('guessright', hidden_word, 'after alter hidden word')
 
     def alter_hidden_word(self, letter, split_word, guess_count, guess_letters, hidden_word, data, random_word):
 
@@ -123,16 +118,15 @@ class Game():
         print('display hidden word ran')
         hidden_word = [self.alter_hidden_word(letter, split_word, guess_count, guess_letters, hidden_word, data, random_word)
                        for letter in split_word]
-
+        hidden_word = "".join(hidden_word)
         self.validate_input(split_word, guess_count,
                             hidden_word, guess_letters, data, random_word)
-
         return hidden_word
 
     def guess_wrong(self, letter, split_word, guess_count, hidden_word, guess_letters, data, random_word):
         guess_count = guess_count - 1
 
-        while guess_count > 0:
+        if guess_count > 0:
 
             if letter in ANWORDS:
                 print(f'Sorry, the word does not have an {letter}!')
@@ -149,13 +143,13 @@ class Game():
 
     def won_game(self, data):
 
-        print('Congratulations! You won the game! Bask in it for a few seconds.)
+        print('Congratulations! You won the game! Bask in it for a few seconds.')
         print('Would you like to play again?')
         answer = input('Please enter "y" for "yes" or press ANY key to quit')
 
         if answer.lower() == "y":
 
-            self.random_word(data))
+            self.random_word(data)
 
         else:
 
@@ -166,7 +160,7 @@ class Game():
 
         print(
             f"You lost this time! \nThe hidden word was {random_word}. \nWould you like to play again? ")
-        answer=input('Please enter "y" for "yes" or press ANY key to quit')
+        answer = input('Please enter "y" for "yes" or press ANY key to quit')
 
         if answer.lower() == "y":
 
